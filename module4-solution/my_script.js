b0954955707951741f9c7d279c455550a54f6ea4
omid-nourani <omid.nourani@yahoo.com>
2021-07-01 22:31:42 +0430

    My solution for the assignment of module 4

---
+++ module4-solution/my_script.js
@@ -0,0 +1,13 @@
+(function(window) {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0);
+
+  if ("j" == firstLetter.toLowerCase()) {
+    window.byeSpeaker.speak(names[i]);
+  } else {
+    window.helloSpeaker.speak(names[i]);
+  }
+} })(window);
